ed286d77e93795a705854085cc7c8bd583fcd637
lvyaojia <lvyaojia@gmail.com>
2012-09-07 19:41:04 +0800

    finish the basic part of muti-room chatting

---
+++ version2/Server/MatesServer.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+"""
+Server side for Mates.
+handle chat info using Websocket.
+"""
+
+import logging
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+
+from tornado.options import define, options
+
+#usage: python MatesServer.py --port=80
+define("port", default=8000, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/(?P<userID>\d+)/(?P<roomID>\d+)/', MainHandler),
+            (r'/chat/(?P<userID>\d+)/(?P<roomID>\d+)/', ChatSocketHandler),
+        ]
+        settings = dict(
+            #定义template 和 static 文件的路径
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, userID, roomID):
+        self.render("index.html", userID=userID, roomID=roomID)
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+
+    #chatrooms, each chatroom has a waiter(user) set
+    activities = {}
+
+    def allow_draft76(self):
+        # for iOS 5.0 Safari
+        return True
+
+    def open(self, userID, roomID):
+        logging.info("%s has connected to room:%s" %(userID, roomID))
+        self.userID = userID
+        self.roomID = roomID
+        waiterSet = ChatSocketHandler.getWaiterSet(roomID)
+        waiterSet.add(self)
+
+    def on_close(self):
+        logging.info("%s has closed at room:%s" %(self.userID, self.roomID))
+        waiterSet = ChatSocketHandler.getWaiterSet(self.roomID)
+        waiterSet.remove(self)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        ChatSocketHandler.send_updates(message, self.roomID)
+
+    @classmethod
+    def getWaiterSet(cls, roomID):
+        waiterSet = cls.activities.get(roomID)
+        if not waiterSet:
+            cls.activities[roomID] = set()
+        return cls.activities[roomID]
+
+    @classmethod
+    def send_updates(cls, message, roomID):
+        logging.info("sending message to %d waiters in Room:%s" % (len(cls.activities.get(roomID)), roomID))
+        waiterSet = cls.activities.get(roomID)
+        for waiter in waiterSet:
+            try:
+                waiter.write_message(message)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
---
+++ version2/Server/static/chat.js
@@ -0,0 +1,58 @@
+// Copyright 2009 FriendFeed
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+$(document).ready(function() {
+    if (!window.console) window.console = {};
+    if (!window.console.log) window.console.log = function() {};
+
+    $("#submit").click(function() {
+        newMessage($("#message").val());
+        return false;
+    });
+
+    $("#messageform").live("keypress", function(e) {
+        if (e.keyCode == 13) {
+            newMessage($("#message").val());
+            return false;
+        }
+    });
+
+    updater.start();
+});
+
+function newMessage(message) {
+    updater.socket.send(message);
+    $("#message").val("").select();
+    console.log('send message: '+ message);
+    return false;
+}
+
+var updater = {
+    socket: null,
+
+    start: function() {
+        var url = "ws://" + location.host + "/chat/" + $("#userID").val() + "/" + $("#roomID").val()+"/";
+        if ("WebSocket" in window) {
+	    updater.socket = new WebSocket(url);
+        } else {
+            updater.socket = new MozWebSocket(url);
+        }
+
+    	updater.socket.onmessage = function(event) {
+            console.log('receive message: '+event.data)
+    	}
+    }
+
+};
+
